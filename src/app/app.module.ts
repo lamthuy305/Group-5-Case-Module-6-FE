d029eb41104a00a09b8e29fa58f71f2abccb379c
CuongMach <tuancuong682003@gmail.com>
2022-05-11 11:45:10 +0700

    f cm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
   declarations: [
