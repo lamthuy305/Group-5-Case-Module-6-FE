c5058ea3128cef982a1cee56a4946b0cff930dd3
unknown <lamthuy305@gmail.com>
2022-05-12 10:44:30 +0700

    reset dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,9 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {AdminLayoutComponent} from './layout/admin/admin-layout/admin-layout.component';
-import {LoginComponent} from './login/login.component';
-import {HomeComponent} from './layout/user/home/home.component';
 
 
 const routes: Routes = [
-  {
-    path: 'admin',
-    component: AdminLayoutComponent,
-    loadChildren: () => import('./admin/admin.module').then(module => module.AdminModule)
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-    loadChildren: () => import('./user/home.module').then(module => module.HomeModule)
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { NavbarComponent } from './layout/admin/navbar/navbar.component';
-import { SidebarComponent } from './layout/admin/sidebar/sidebar.component';
-import { AdminLayoutComponent } from './layout/admin/admin-layout/admin-layout.component';
-import { HomeComponent } from './layout/user/home/home.component';
-import { NavbarHomeComponent } from './layout/user/navbar-home/navbar-home.component';
-import { SidebarHomeComponent } from './layout/user/sidebar-home/sidebar-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    NavbarComponent,
-    SidebarComponent,
-    AdminLayoutComponent,
-    HomeComponent,
-    NavbarHomeComponent,
-    SidebarHomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,4 +19,4 @@ import { SidebarHomeComponent } from './layout/user/sidebar-home/sidebar-home.co
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
