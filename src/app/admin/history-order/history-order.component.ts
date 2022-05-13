42f1ae0e63dced331f005e3f233d497d0c10c770
unknown <lamthuy305@gmail.com>
2022-05-13 15:00:12 +0700

    done orders

--- src/app/admin/history-order/history-order.component.ts
+++ src/app/admin/history-order/history-order.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {OrderService} from '../../service/order/order.service';
 import {Router} from '@angular/router';
 import {NotificationService} from '../../service/notification/notification.service';
+
+declare var $: any;
+declare var Swal: any;
 
 @Component({
   selector: 'app-history-order',
@@ -14,7 +17,7 @@ export class HistoryOrderComponent implements OnInit {
 
   constructor(private orderService: OrderService,
               private router: Router,
-              private notificationService:NotificationService) {
+              private notificationService: NotificationService) {
   }
 
   ngOnInit() {
@@ -30,6 +33,38 @@ export class HistoryOrderComponent implements OnInit {
   getAllOrderStatusDone() {
     this.orderService.getAllOrderStatusDone(this.currentUser.id).subscribe((ordersBE) => {
       this.orders = ordersBE;
+      $(function() {
+        $('#history-order').DataTable({
+          'paging': true,
+          'lengthChange': false,
+          'searching': false,
+          'ordering': true,
+          'info': true,
+          'autoWidth': false,
+        });
+      });
     });
+  }
+
+  deleteOrder(id) {
+    Swal.fire({
+      title: 'Bạn có muốn xóa?',
+      text: 'You wont be able to revert this!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+          this.orderService.deleteOrder(id).subscribe(() => {
+              this.notificationService.showMessage('success', 'Delete!', 'Xóa thành công');
+              this.router.navigateByUrl('/products');
+            }, error =>
+              this.notificationService.showMessage('erros', 'Delete!', 'Xóa lỗi')
+          );
+        }
+      }
+    );
   }
 }
